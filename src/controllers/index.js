499b04ec6a141b06354cb2d775a9da8b317436b0
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-10-14 10:24:27 -0700

    registro de usuarios en BD con el password hasheado

---
+++ src/controllers/index.js
@@ -0,0 +1,32 @@
+let User = require('../models').UserModel;
+let bcrypt = require('bcryptjs');
+
+let signUp = async (req, res, next) => {
+    const { password, email, name, role } = req.body;
+    try {
+        await User.findOne({
+            where: {
+                email: email
+            }
+        }
+        ).then(async user => {
+            if (user) {
+                return ('El usuario ya existe')
+            }
+            let newUser = await User.create({
+                email,
+                password: await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS)),
+                role: parseInt(role),
+                name
+            })
+            return newUser;
+        }).then(data => {
+            res.send(data)
+        })
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
+module.exports = signUp;

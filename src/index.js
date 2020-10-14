9510de7399ee50421abc25028e63c22899aa8b08
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-10-14 10:14:53 -0700

    modelos y rutas de usuarios y solicitudes devididas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const app = express();
-const endpoints = require('./routes');
+const loginRoutes = require('./routes/login');
+const requestCredits = require('./routes/solicitudes');
 const cors = require('cors');
+const models = require('./models');
+const passport = require('passport');
 
+require('./helpers/passport');
+
+console.log(process.env.DBMYSQL)
+
+app.use(passport.initialize());
 const port = process.env.PORT || 81;
 app.set('port', process.env.PORT || port);
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors({ origin: true }));
-
-app.use('/', endpoints);
+app.use('/api', loginRoutes);
+app.use('/api/req', requestCredits);
 app.listen(app.get('port'), () => {
     console.log('Servidor iniciado en puerto ' + port)
 });

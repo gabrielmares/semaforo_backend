a340d4ea7a13a5f8875fea0e273c832b5b0f56ae
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-10-15 14:09:47 -0700

    organizando rutas de la api

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,33 +1,7 @@
-let User = require('../models').UserModel;
-let bcrypt = require('bcryptjs');
+let signup = require('./signup');
+let signin = require('./signin');
 
-
-let signUp = async (req, res, next) => {
-    const { password, email, name, role } = req.body;
-    try {
-        await User.findOne({
-            where: {
-                email: email
-            }
-        }
-        ).then(async user => {
-            if (user) {
-                return ('El usuario ya existe')
-            }
-            let newUser = await User.create({
-                email,
-                password: await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS)),
-                role: parseInt(role),
-                name
-            })
-            return newUser;
-        }).then(data => {
-            res.send(data)
-        })
-    } catch (error) {
-        console.log(error)
-        throw error
-    }
+module.exports = {
+    signin,
+    signup
 }
-
-module.exports = signUp;

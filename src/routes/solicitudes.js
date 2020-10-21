a3245f43622e8fc6c4d5bc9d4b49a2aa85a70d04
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-10-21 08:59:41 -0700

    lectura de datos desde Mysql

--- src/routes/solicitudes.js
+++ src/routes/solicitudes.js
@@ -1,6 +1,8 @@
 var router = require('express').Router();
-let pool = require('../firebird');
 let { promesa } = require('../querys');
+const { requestsToRenovate } = require('../models');
+const { Op, QueryTypes } = require('sequelize');
+const sequelize = require('sequelize')
 
 
 let query = {};
@@ -16,63 +18,43 @@ query.ACTIVIDAD = 'select NOMBRE as ACTIVIDAD from CLIAC_ACTIVIDADES where CODIG
 query.ULTIMOCREDITO = "select sol.MONTOSOL as Monto from CCMOV_MOVIMIENTOS mov join FSOCR_SOLICITUDES sol on sol.NOCONTRATO=mov.FACTURA where mov.CLIENTE=? and mov.TIPO=101 and mov.IMPORTE>20 order by mov.FECVTO desc rows 1"
 query.ASESOR = 'select NOMBRE from direc_directorio where CODIGO=?'
 
-router.get('/listas', (req, res) => {
-    // const { filter } =
-    const { sucursal, from, to, lastCredit, centro } = req.query;
-    console.log(req.query)
-    // console.table(sucursal, from, to, lastCredit);
-    if (!sucursal) return res.send(false)
-    pool.get(function (err, db) {
-        if (err) {
-            throw err;
-        }
-        console.log('Iniciando query')
-        // llamada a la bd, obtiene la lista de creditos a vencer en los futuros 15 dias
+router.get('/listas', async (req, res) => {
+    let { CENTRO, FINNOSUCURSAL, DESDE, HASTA } = req.query;
+    let rows = {};
+    console.log(req.query);
+    if (CENTRO === 0) {
         try {
-            db.query(`SELECT su.NOMBRE as Sucursal, C.CODIGO,  c.NOMBRE as Cliente, a.CENTRO, a.GRUPO, a.NOCONTRATO as Contrato, a.MONTOSOL as Credito, s.FECVTO as Vencimiento, sal.saldo, sal.sdocapital, sal.ultimo, ((a.MONTOSOL - sal.sdocapital)/(a.MONTOSOL))*100 as PorcPagado 
-            FROM FSOCR_SOLICITUDES a  join CCSDO_SALDOS s on a.NOCONTRATO=s.FACTURA and a.NOPAGOS=s.PLAZO and a.STATUS=2 and a.CENTRO=${centro}
-            join SUCUR_SUCURSALES su
-            on a.NOSUCURSAL=su.CODIGO 
-            join CLIEN_CLIENTES c on 
-            c.CODIGO=a.CLIENTE 
-            inner join (select c.finnosucursal, c.fincentro, c.codigo, c.fingrupo, sol.solfecha, sol.montosol, sol.feccontrato, c.nombre as nomcli, f.factura, f.sdocapital, (f.sdocapital+f.sdointeres+ f.sdoivainteres+ f.sdomancta) as saldo, ult.ultimo 
-            from 
-            (select  m.cliente, m.factura, 
-            sum(case when co.CARGOABONO = 'A' then m.MNCAPITAL * (-1) else m.MNCAPITAL end) as sdocapital, 
-            sum(case when co.CARGOABONO = 'A' then m.mnmontointeres * (-1) else m.mnmontointeres end) as sdointeres,
-            sum(case when co.CARGOABONO = 'A' then m.mnmontoivaint * (-1) else m.mnmontoivaint end) as sdoivainteres, 
-            sum(case when co.CARGOABONO = 'A' then (m.mnmontomanejocta + m.mnmontoivamancta) * (-1) else (m.mnmontomanejocta + m.mnmontoivamancta) end) as sdomancta,
-            sum(case when co.CARGOABONO = 'A' then m.mnmontofongar * (-1) else m.mnmontofongar end) as fongar, 
-            sum(case when co.CARGOABONO = 'A' then m.mnimporte * (-1) else m.mnimporte end) as IMPORTE 
-            from CCMOV_MOVIMIENTOS m 
-            left outer join CCCON_CONCEPTOS co 
-            on(co.CODIGO = m.TIPO) 
-            join FSOCR_SOLICITUDES ant on (ant.NOCONTRATO=m.FACTURA and ant.SOLFECHA<${lastCredit})
-            where m.FECHA between 20180101 and ${to} and m.FACTURA <> 'FONGAR'  
-            group by m.CLIENTE, m.FACTURA)    f 
-            left outer join FSOCR_SOLICITUDES sol 
-            on(f.cliente = sol.CLIENTE and f.FACTURA = sol.NOCONTRATO) 
-            left outer join CLIEN_CLIENTES c 
-            on(f.cliente = c.CODIGO) 
-            left outer join SUCUR_SUCURSALES s  on(c.FINNOSUCURSAL = s.CODIGO) 
-        left outer join FCENT_CENTROS ce on(c.FINCENTRO = ce.CENTRO and c.FINNOSUCURSAL = ce.NOSUCURSAL)
-        join (select m.CLIENTE, m.FACTURA, max(m.FECHA) as ultimo from CCMOV_MOVIMIENTOS m where m.TIPO=101 group by m.CLIENTE, m.FACTURA) ult
-        on (c.CODIGO=ult.cliente and f.factura=ult.factura) where(f.importe < -0.01 or f.importe > 0.01)  And ((c.finnosucursal =${sucursal}) and sol.SOLFECHA<${lastCredit})  
-        order by c.FINNOSUCURSAL, c.FINCENTRO, c.FINGRUPO, f.cliente ) sal on (sal.codigo = c.CODIGO and sal.factura=a.nocontrato)
-        where a.STATUS = 2 and s.FECVTO>=${from} and s.FECVTO <=${to} order by su.NOMBRE, a.CENTRO, s.FECVTO `, function (err, rows) {
-                if (err) {
-                    throw err;
-                }
-                db.detach();
-                console.log('retornando query al cliente')
-                console.table(rows)
-                return res.send(rows)
-            })
+            rows = await requestsToRenovate
+                .findAll({
+                    where: {
+                        [Op.and]: [{ FINNOSUCURSAL: parseInt(FINNOSUCURSAL) }, { CENTRO: parseInt(CENTRO) }],
+                    },
+                })
+            console.log(rows[0]);
+            return res.send(rows)
         } catch (error) {
-            console.log(error)
-            return res.send(error)
+            return console.log(error)
         }
-    })
+    }
+
+    try {
+        rows = await requestsToRenovate
+            .findAll({
+                where: {
+                    FINNOSUCURSAL: parseInt(FINNOSUCURSAL),
+                    VENCIMIENTO: {
+                        [Op.gte]: [DESDE],
+                        [Op.lte]: [HASTA]
+                    }
+
+                },
+            })
+        console.log(rows[0]);
+        return res.send(rows)
+    } catch (error) {
+        return console.log(error)
+    }
+
 })
 
 
@@ -106,3 +88,61 @@ router.get('/renovacion', async (req, res) => {
 
 module.exports = router;
 
+/*
+
+const { sucursal, from, to, lastCredit, centro } = req.query;
+    console.log(req.query)
+    // console.table(sucursal, from, to, lastCredit);
+    if (!sucursal) return res.send(false)
+    pool.get(function (err, db) {
+        if (err) {
+            throw err;
+        }
+        console.log('Iniciando query')
+        // llamada a la bd, obtiene la lista de creditos a vencer en los futuros 15 dias
+        try {
+            db.query(`SELECT su.NOMBRE as Sucursal, C.CODIGO,  c.NOMBRE as Cliente, a.CENTRO, a.GRUPO, a.NOCONTRATO as Contrato, a.MONTOSOL as Credito, s.FECVTO as Vencimiento, sal.saldo, sal.sdocapital, sal.ultimo, ((a.MONTOSOL - sal.sdocapital)/(a.MONTOSOL))*100 as PorcPagado
+            FROM FSOCR_SOLICITUDES a  join CCSDO_SALDOS s on a.NOCONTRATO=s.FACTURA and a.NOPAGOS=s.PLAZO and a.STATUS=2 and a.CENTRO=${centro}
+            join SUCUR_SUCURSALES su
+            on a.NOSUCURSAL=su.CODIGO
+            join CLIEN_CLIENTES c on
+            c.CODIGO=a.CLIENTE
+            inner join (select c.finnosucursal, c.fincentro, c.codigo, c.fingrupo, sol.solfecha, sol.montosol, sol.feccontrato, c.nombre as nomcli, f.factura, f.sdocapital, (f.sdocapital+f.sdointeres+ f.sdoivainteres+ f.sdomancta) as saldo, ult.ultimo
+            from
+            (select  m.cliente, m.factura,
+            sum(case when co.CARGOABONO = 'A' then m.MNCAPITAL * (-1) else m.MNCAPITAL end) as sdocapital,
+            sum(case when co.CARGOABONO = 'A' then m.mnmontointeres * (-1) else m.mnmontointeres end) as sdointeres,
+            sum(case when co.CARGOABONO = 'A' then m.mnmontoivaint * (-1) else m.mnmontoivaint end) as sdoivainteres,
+            sum(case when co.CARGOABONO = 'A' then (m.mnmontomanejocta + m.mnmontoivamancta) * (-1) else (m.mnmontomanejocta + m.mnmontoivamancta) end) as sdomancta,
+            sum(case when co.CARGOABONO = 'A' then m.mnmontofongar * (-1) else m.mnmontofongar end) as fongar,
+            sum(case when co.CARGOABONO = 'A' then m.mnimporte * (-1) else m.mnimporte end) as IMPORTE
+            from CCMOV_MOVIMIENTOS m
+            left outer join CCCON_CONCEPTOS co
+            on(co.CODIGO = m.TIPO)
+            join FSOCR_SOLICITUDES ant on (ant.NOCONTRATO=m.FACTURA and ant.SOLFECHA<${lastCredit})
+            where m.FECHA between 20180101 and ${to} and m.FACTURA <> 'FONGAR'
+            group by m.CLIENTE, m.FACTURA)    f
+            left outer join FSOCR_SOLICITUDES sol
+            on(f.cliente = sol.CLIENTE and f.FACTURA = sol.NOCONTRATO)
+            left outer join CLIEN_CLIENTES c
+            on(f.cliente = c.CODIGO)
+            left outer join SUCUR_SUCURSALES s  on(c.FINNOSUCURSAL = s.CODIGO)
+        left outer join FCENT_CENTROS ce on(c.FINCENTRO = ce.CENTRO and c.FINNOSUCURSAL = ce.NOSUCURSAL)
+        join (select m.CLIENTE, m.FACTURA, max(m.FECHA) as ultimo from CCMOV_MOVIMIENTOS m where m.TIPO=101 group by m.CLIENTE, m.FACTURA) ult
+        on (c.CODIGO=ult.cliente and f.factura=ult.factura) where(f.importe < -0.01 or f.importe > 0.01)  And ((c.finnosucursal =${sucursal}) and sol.SOLFECHA<${lastCredit})
+        order by c.FINNOSUCURSAL, c.FINCENTRO, c.FINGRUPO, f.cliente ) sal on (sal.codigo = c.CODIGO and sal.factura=a.nocontrato)
+        where a.STATUS = 2 and s.FECVTO>=${from} and s.FECVTO <=${to} order by su.NOMBRE, a.CENTRO, s.FECVTO `, function (err, rows) {
+                if (err) {
+                    throw err;
+                }
+                db.detach();
+                console.log('retornando query al cliente')
+                console.table(rows)
+                return res.send(rows)
+            })
+        } catch (error) {
+            console.log(error)
+            return res.send(error)
+        }
+    })
+*/

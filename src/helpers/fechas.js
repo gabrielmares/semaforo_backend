e147539d6912208e6515f639166d695719b3883d
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-10-26 09:05:28 -0700

    helpers para fechas

--- src/helpers/fechas.js
+++ src/helpers/fechas.js
@@ -1,13 +1,50 @@
-/*FUNCION QUE TOMA LA FECHA PROVENIENTE DE NACIMIENTO DE LA BASE DE DATOS Y LA FORMATEA "DD/MM/AAAA" PARA LA VISTA DE LA TABLA INGRESOS */
+
 function ParseDate(fecnac) {
     let arr = new String(fecnac);
     arr = Array.from(arr);
     let yearI = arr.slice(0, 4);
     let MonthI = arr.slice(4, 6);
     let dayI = arr.slice(6, 8);
-    // return FecNacioParse;
     return (dayI.join("") + "/" + MonthI.join("") + "/" + yearI.join(""))
 
 }
 
-module.exports.ParseDate = ParseDate;
+function CambiarFecha(FechaDeEntrada) {
+    let FechaRegistrada = FechaDeEntrada;
+    if (isNaN(FechaRegistrada)) {
+        FechaRegistrada = Date.now();
+    }
+    let formatter = new Intl.DateTimeFormat('es-MX', {
+        weekday: 'long',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true,
+        timeZone: 'UTC'
+    });
+    let NuevaFecha = formatter.formatToParts(FechaRegistrada);
+    let NuevaFechaParse = NuevaFecha[0].value + "" + NuevaFecha[2]["value"] + "" + NuevaFecha[4]["value"];
+    return NuevaFechaParse;
+}
+
+function Inputdate(fecnac) {
+    // eslint-disable-next-line
+    let arr = new String(fecnac);
+    arr = Array.from(arr);
+    arr.splice(4, 0, "-");
+    arr.splice(7, 0, "-");
+    let FecNacioParse = arr.join("");
+    return FecNacioParse;
+}
+
+
+
+
+module.exports = {
+    Inputdate,
+    ParseDate,
+    CambiarFecha
+};

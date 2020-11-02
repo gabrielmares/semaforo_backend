90447212401da082471d1246daa9f3d1043a9d3a
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-11-02 09:08:32 -0700

    headers para peticiones y firma de cookies

--- src/index.js
+++ src/index.js
@@ -6,20 +6,31 @@ const cors = require('cors');
 const models = require('./models');
 const passport = require('passport');
 const update = require('./helpers/cronjobs');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 
-
+require('dotenv').config();
 require('./helpers/passport');
 
 
-// app.use(cron());
+app.use(morgan('combined'));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
+app.use(cookieParser(process.env.SECRETCOOKIES));
+app.use(helmet());
 app.use(passport.initialize());
 const port = process.env.PORT || 81;
 app.set('port', process.env.PORT || port);
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({ origin: true }));
 app.use('/api', loginRoutes);
-app.use('/api/req', requestCredits);
+app.use('/api/operaciones', requestCredits);
 app.listen(app.get('port'), () => {
     console.log('Servidor iniciado en puerto ' + port)
 });

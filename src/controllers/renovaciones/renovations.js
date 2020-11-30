2bae13acd80d132ea4ee403b584140a05d240a5d
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-11-30 09:08:05 -0700

    query necesario en el dashboard para mostrar los registros por sucursal

---
+++ src/controllers/renovaciones/renovations.js
@@ -0,0 +1,34 @@
+let { Renovations, RequestsInProcess } = require('../../models');
+
+
+// funcion utilizada en el dashbord de la vista
+// obtiene los registros que el tipo de usuario necesita
+// en la vista son filtrados
+const Totales = async (req, res) => {
+    let rows = {};
+    try {
+        if ((parseInt(req.query.sucursal)) > 0) {
+            console.log('filtrando por sucursal')
+            rows.renovations = await Renovations.findAll({
+                where: {
+                    FINNOSUCURSAL: parseInt(req.query.sucursal)
+                }
+            })
+            rows.requests = await RequestsInProcess.findAll({
+                where: {
+                    sucursal: parseInt(req.query.sucursal)
+                }
+            })
+        } else {
+            console.log('filtrando sin sucursal')
+            rows.renovations = await Renovations.findAll();
+            rows.requests = await RequestsInProcess.findAll();
+        }
+        return res.send(rows)
+    } catch (error) {
+        return res.sendStatus(607)
+    }
+
+}
+
+module.exports = Totales;
